13f64811ddfad2463b737a73ee22d03ae0db33d7
Abdelmadjid Cherfaoui <abdelmadjid.cherfaoui@gmail.com>
2024-02-02 14:26:49 -0500

    fix the build

--- src/app/component/shared/header/header.component.ts
+++ src/app/component/shared/header/header.component.ts
@@ -1,52 +1,48 @@
-import { Component } from "@angular/core";
-import { Router,NavigationEnd  } from '@angular/router';
-import { filter, map } from "rxjs";
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
-  styleUrl: "./header.component.scss",
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   currentRoute: string | undefined;
   isVisible = false;
   activatedRoute: any;
   currentUrl: any;
-  isConnected =false;
-  navbarOpen=false;
+  isConnected = false;
+  navbarOpen = false;
   navigationItems = [
-    { text: "About us", href: "/aboutUs" , selected:"" },
-    { text: "Why Talent Check", href: "/aboutUs#WhyTalent", selected:"" },
-    { text: "How it works", href: "/aboutUs#HowItWorks", selected:"" },
-    { text: "Trusted by", href: "/aboutUs#TrustedBy" , selected:""},
-    { text: "Plans", href: "/aboutUs#Plans" , selected:""},
-    { text: "Resources", href: "/aboutUs#Ressources", selected:"" },
-    { text: "Contact us", href: "/aboutUs#ContactUs" , selected:""},
+    { text: 'About us', href: '/aboutUs', selected: '' },
+    { text: 'Why Talent Check', href: '/aboutUs#WhyTalent', selected: '' },
+    { text: 'How it works', href: '/aboutUs#HowItWorks', selected: '' },
+    { text: 'Trusted by', href: '/aboutUs#TrustedBy', selected: '' },
+    { text: 'Plans', href: '/aboutUs#Plans', selected: '' },
+    { text: 'Resources', href: '/aboutUs#Ressources', selected: '' },
+    { text: 'Contact us', href: '/aboutUs#ContactUs', selected: '' },
   ];
 
-  constructor(private router: Router){
-    this.router.events
-  .subscribe((event) => {
-    if (event instanceof NavigationEnd) {
-      this.currentUrl = event.url;
-      this.navigationItems.map((item)=>{
-        if(item.href == this.currentUrl){
-          item.selected="selected";
-        }
-        else{
-          item.selected="";
-        }
-      });
-
-    }
-  })
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl = event.url;
+        this.navigationItems.map((item) => {
+          if (item.href == this.currentUrl) {
+            item.selected = 'selected';
+          } else {
+            item.selected = '';
+          }
+        });
+        console.log('Current route', this.currentUrl);
+      }
+    });
 
     let display = localStorage.getItem('showLogin');
-    if(display != null){
-      this.isVisible = display !="true";
+    if (display != null) {
+      this.isVisible = display != 'true';
     }
   }
-  toggleNavbar(){
+  toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
-
 }

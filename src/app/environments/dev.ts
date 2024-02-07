af112133b9c57923ef8c3a4fbf9dbe11c62f8531
Abdelmadjid Cherfaoui <abdelmadjid.cherfaoui@gmail.com>
2024-02-07 08:44:10 -0500

    updates

--- src/app/environments/dev.ts
+++ src/app/environments/dev.ts
@@ -1,4 +1,5 @@
 export const dev = {
   production: false,
-  apiUrl: 'http://localhost:3030/api/v1',
+  apiUrl:
+    'http://talentchecks-backend-lb-1217970925.eu-central-1.elb.amazonaws.com:3030/api/v1',
 };
